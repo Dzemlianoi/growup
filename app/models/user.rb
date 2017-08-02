85b578fe17ab3f95977b73f74e0f5364b12ec137
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-08-02 20:16:03 +0300

    initialize home page

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable,
+   omniauth_providers: %i(facebook github twitter google)
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.image = auth.info.image # assuming the user model has an image
+      # user.name = auth.info.name   # assuming the user model has a name
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,7 @@
+# Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.mailer_sender = 'reply-growup@gmail.com'
+  config.mailer_sender = 'test@test.com'
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
@@ -7,6 +9,9 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 11
   config.reconfirmable = true
   config.expire_all_remember_me_on_sign_out = true
+  config.password_length = 6..128
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  config.reset_password_within = 6.hours
   config.sign_out_via = :delete
+  config.omniauth :facebook, ENV['FB_ID'], ENV['FB_SECRET']
 end
