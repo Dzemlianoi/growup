2993895c167cd576229208465f6b1414b294a9ff
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-08-02 22:11:44 +0300

    add confirmed_at to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       # user.image = auth.info.image # assuming the user model has an image
       # user.name = auth.info.name   # assuming the user model has a name
+    end
   end
 
   def self.new_with_session(params, session)
