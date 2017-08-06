ee19779d5b8c8bd4fa88b0c663681c799ecddc81
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-08-06 11:27:11 +0300

    courses implemented

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    binding.pry
     user ||= User.new
     can :manage, :all if user.admin?
     can :read, :all if user.confirmed?
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802191016) do
+ActiveRecord::Schema.define(version: 20170806082228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.text "text"
+    t.boolean "is_active", default: false
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: ""
