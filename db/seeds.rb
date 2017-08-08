3e0eba963e374db0e49c43c871db88aeefa72c43
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-08-08 16:26:29 +0300

    fixes by layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,15 @@ def courses
   [
     {
       title: 'Intro',
-      description: 'Why i am here? Why i should be in IT? Why, why, why? Here is an answer!',
+      description: 'Why i am here? Why i should be in IT? So many why? Here is an answer!',
+    },
+    {
+      title: 'GIT & other SCV',
+      description: 'Main tool to make your code safe. Add to it some versioning. Flavor it with coworking ability. The basis of basises'
     },
     {
       title: 'HTML',
-      description: 'Like alphabet starts with A, Web development starts with HTML.'
+      description: 'Like alphabet starts with "A", Web development starts with HTML. Meet it!'
     },
     {
       title: 'CSS',
@@ -14,7 +18,7 @@ def courses
     },
     {
       title: 'Bootstrap',
-      description: 'Bootstrap. Bootstrap it. Bootstrap it, faster.'
+      description: 'Bootstrap. Bootstrap it. Bootstrap it, faster. CSS-easy way lib'
     },
     {
       title: 'JavaScript',
@@ -22,7 +26,7 @@ def courses
     },
     {
       title: 'JQuery',
-      description: 'WHY YOU HAVENT SHOW IT TO ME BEFORE?!?!?1 Its so yummy, yummy, yummy'
+      description: 'Why you havent show it to me before?!?!?1 Its so yummy, yummy, yummy'
     },
     {
       title: 'Test your code! Why? How? When?',
@@ -31,6 +35,34 @@ def courses
     {
       title: 'JavaScript libs and frameworks',
       description: 'Man shall not live by JavaScript alone. Here is a pull of the most powerfull and modern libs whith small description'
+    },
+    {
+      title: 'Refactoring',
+      description: 'Sometimes you will never get the rubish out if nobody says that its rubish! Clean your code! Different approaches for the main idea.'
+    },
+    {
+      title: 'MySQL',
+      description: 'The system of database management. Create/Destroy/Update/Delete your data immediately. Select all clasters of data you want in few seconds.'
+    },
+    {
+      title: 'Linux vs Terminal',
+      description: 'Thats Linux Torvald pink world. Discover main commands and utils of Linux'
+    },
+    {
+      title: 'Ruby',
+      description: 'Welcome to BackEnd mine of Ruby. Here we start to deal with it!'
+    },
+    {
+      title: 'Rails',
+      description: 'Top Ruby framework. Nothing else.'
+    },
+    {
+      title: 'TDD vs BDD',
+      description: 'Methodologies that develop yourself and open eyes for another workflow of development'
+    },
+    {
+      title: 'React.js',
+      description: 'Reactify yourself. Top JS framework of 2k17'
     }
   ].each_with_index do |project, key|
     begin
@@ -45,5 +77,6 @@ def courses
     end
   end
 end
+
 
 courses
