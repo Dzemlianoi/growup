9a003c4316d2d53dc59d63f7dba4fde66aa6d5eb
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-08-12 21:20:43 +0300

    rollback with active admin

--- config/initializers/active_admin.rb
+++
@@ -1,9 +0,0 @@
-ActiveAdmin.setup do |config|
-  config.site_title = "Growup Courses"
-  config.authentication_method = :authenticate_user!
-  config.current_user_method = :current_user
-  config.logout_link_path = :destroy_user_session_path
-  config.batch_actions = true
-  config.localize_format = :long
-  config.comments = false
-end
