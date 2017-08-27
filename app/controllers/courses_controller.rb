a5ccb8bbd767059cd7e0ccc272e647fbf51d516a
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-08-27 10:50:15 +0300

    fix layout for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
   load_and_authorize_resource find_by: :slug
 
   def index
-    @courses = @courses.active.by_order
+    @courses = @courses.by_order
   end
 
   def show; end
